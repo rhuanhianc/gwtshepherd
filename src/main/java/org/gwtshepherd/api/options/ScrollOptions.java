4022864dc14c769508c8e755087cee1736585d1d
rhuan.hianc <rhuanhianc123@gmail.com>
2023-05-02 00:22:18 -0300

    Update ScrollOptions.java

--- src/main/java/org/gwtshepherd/api/options/ScrollOptions.java
+++ src/main/java/org/gwtshepherd/api/options/ScrollOptions.java
@@ -9,12 +9,13 @@ import jsinterop.annotations.JsType;
  * @author Rhuan Hianc
  */
 
-@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
-public class ScrollOptions {
-
-    @JsProperty
-    public native void setBehavior(String behavior);
-
-    @JsProperty
-    public native void setBlock(String block);
-}
+ @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+ public class ScrollOptions {
+ 
+     @JsProperty
+     public native void setBehavior(String behavior);
+ 
+     @JsProperty
+     public native void setBlock(String block);
+ 
+    }
